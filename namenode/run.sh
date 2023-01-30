e8bcfdad6024adade3b2122a584e3ffdfab69ca1
o2183251@campus.univ-orleans.fr <o2183251@campus.univ-orleans.fr>
2023-01-30 17:41:09 +0100

    setup hive

--- namenode/run.sh
+++ namenode/run.sh
@@ -2,8 +2,16 @@
 apt-get update
 apt-get install wget
 wget --no-check-certificate https://dlcdn.apache.org/pig/pig-0.16.0/pig-0.16.0.tar.gz
+wget --no-check-certificate https://dlcdn.apache.org/hbase/2.5.2/hbase-2.5.2-src.tar.gz
+wget --no-check-certificate https://dlcdn.apache.org/hive/stable-2/apache-hive-2.3.9-bin.tar.gz
 tar xvzf pig-0.16.0.tar.gz
+tar xvzf hbase-2.5.2-src.tar.gz
+tar xvzf apache-hive-2.3.9-bin.tar.gz
 mv ./pig-0.16.0 /root/pig
+mv ./hbase-2.5.2 /root/hbase
+mv ./apache-hive-2.3.9-bin  /root/hive
+rm /root/hive/lib/guava-14.0.1.jar
+cp /opt/hadoop-3.2.1/share/hadoop/common/lib/guava-27.0-jre.jar /root/hive/lib/
 
 namedir=`echo $HDFS_CONF_dfs_namenode_name_dir | perl -pe 's#file://##'`
 if [ ! -d $namedir ]; then
